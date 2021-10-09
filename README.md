26e8f2d8ab1e8e976e0fa2b38ccffd56cb38cc1f
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2021-10-10 01:01:42 +0700

    Add Unit Test and Readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # bibit-coding-test
  
+Berikut adalah daftar isi dari petunjuk pengecekan soal tes Bibit menggunakan Go
+
+- [No. 1: Simple SQL Command]
+- [No. 2: Go REST API dan gRPC Menggunakan API dari OMDB](#go-omdb)
+- [No. 3: Refactor Code Go]
+- [No. 4: Klasifikasi Kata Anagram]
+
+***
+
+### <a name="prep"></a>No. 2: Go REST API dan gRPC Menggunakan API dari OMDB
+
+Untuk menjalankan program ini, diperlukan melakukan instalasi dengan menjalankan perintah berikut
+
+    cd no2
+    glide install -v
+
+Lalu untuk menjalankan server dapat menggunakan perintah berikut
+
+    go run app/main.go
+
+Untuk pengecekan gRPC, dapat menjalankan program client dengan perintah berikut
+
+    go run app/grpc_client/main.go
+
+Untuk pengecekan unit test, dapat dilakukan dengan perintah berikut
+
+    cd movie/usecase
+    go test
--- no2/movie/usecase/move_ucase_test.go
+++ no2/movie/usecase/move_ucase_test.go
@@ -1,61 +1,72 @@
 package usecase_test
 
 import (
-	"reflect"
+	"go_bibit_test/domain"
+	mocks "go_bibit_test/mocks/domain"
+	"go_bibit_test/movie/usecase"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestFetch(t *testing.T) {
-	tests := []struct {
-		searchword string
-		pagination int64
-		want       *Response
-		wantErr    bool
-	}{
-		{
-			"url exist",
-			"https://www.andanhm.me/gounittest.json",
-			&Response{
-				Name:    "gounittest",
-				Version: "v1.0.0",
-				Status:  true,
-			},
-			false,
-		},
-		{
-			"url not exist",
-			"https://www.andanhm.me/not_exist.json",
-			nil,
-			true,
-		},
-		{
-			"url provided invalid",
-			"andanhm.me/not_exist.json",
-			nil,
-			true,
-		},
-		{
-			"expected json parser error",
-			"https://www.andanhm.me/invalid.json",
-			nil,
-			true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			response, err := Curl(tt.url)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Curl() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(response, tt.want) {
-				t.Errorf("Curl() error = %v, want %v", err, tt.want)
-				return
-			}
-		})
-	}
+	mockLogRepo := new(mocks.LogRepository)
+	mockListOfMovies := make([]domain.Movie, 0)
+	mockMovieUsecase := new(mocks.MovieUsecase)
+
+	t.Run("success", func(t *testing.T) {
+		mockLogRepo.On("Store", mock.Anything, mock.Anything).Return(nil).Once()
+		mockMovieUsecase.On("Fetch", mock.AnythingOfType("string"),
+			mock.AnythingOfType("int64")).Return(mockListOfMovies, nil).Once()
+
+		mucase := usecase.NewMovieUsecase(mockLogRepo, time.Second*2)
+		response, err := mucase.Fetch("Batman", 1)
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, response)
+	})
+
+	t.Run("error-failed", func(t *testing.T) {
+		mockLogRepo.On("Store", mock.Anything, mock.Anything).Return(nil).Once()
+		mockMovieUsecase.On("Fetch", mock.AnythingOfType("string"),
+			mock.AnythingOfType("int64")).Return(mockListOfMovies, nil).Once()
+
+		mucase := usecase.NewMovieUsecase(mockLogRepo, time.Second*2)
+		response, err := mucase.Fetch("", 1)
+
+		assert.Error(t, err)
+		assert.Empty(t, response)
+	})
 }
 
 func TestGetByID(t *testing.T) {
+	mockLogRepo := new(mocks.LogRepository)
+	mockListOfMovies := make([]domain.Movie, 0)
+	mockMovieUsecase := new(mocks.MovieUsecase)
 
+	t.Run("success", func(t *testing.T) {
+		mockLogRepo.On("Store", mock.Anything, mock.Anything).Return(nil).Once()
+		mockMovieUsecase.On("Fetch", mock.AnythingOfType("string"),
+			mock.AnythingOfType("int64")).Return(mockListOfMovies, nil).Once()
+
+		mucase := usecase.NewMovieUsecase(mockLogRepo, time.Second*2)
+		response, err := mucase.GetByID("tt2313197")
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, response)
+	})
+
+	t.Run("error-failed", func(t *testing.T) {
+		mockLogRepo.On("Store", mock.Anything, mock.Anything).Return(nil).Once()
+		mockMovieUsecase.On("Fetch", mock.AnythingOfType("string"),
+			mock.AnythingOfType("int64")).Return(mockListOfMovies, nil).Once()
+
+		mucase := usecase.NewMovieUsecase(mockLogRepo, time.Second*2)
+		response, err := mucase.GetByID("12345")
+
+		assert.Error(t, err)
+		assert.Empty(t, response)
+	})
 }
